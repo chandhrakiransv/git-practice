a92e0c795836495f1661771d3328581da2ba3ac2
Chandhrakiran S V <chandhrakiran.sv@gmail.com>
2026-07-27 08:42:17 +0530

    Rotate90 degree

---
+++ Rotate90.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+class Rotate90{
+
+    public void rotate(int[][] matrix) {
+
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i; j < n; j++) {
+
+                int temp = matrix[i][j];
+
+                matrix[i][j] = matrix[j][i];
+
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse every row
+        for (int i = 0; i < n; i++) {
+
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int temp = matrix[i][left];
+
+                matrix[i][left] = matrix[i][right];
+
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        Rotate90 obj = new Rotate90();
+
+        obj.rotate(matrix);
+
+        // Print the rotated matrix
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
